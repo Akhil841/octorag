d9b6915e8e3e47c2ef3803d45b98a394840fe93e
Akhil Pillai <avpillai@ucsd.edu>
2025-07-14 13:40:02 -0700

    Add test

---
+++ octorag_mcp_test.py
@@ -0,0 +1,18 @@
+from octorag_mcp_client import OctoRAG_MCP
+import asyncio
+
+model = OctoRAG_MCP()
+
+
+async def main():
+    async for msg in model.query(
+        (
+            "Write code in Python to create a HTTP server that can handle GET and POST requests"
+            " It should be able to retrieve stats of NBA players from the NBA API when asked."
+            " And it should predict the probability of a team winning a game against another team, based on the stats of the players."
+        )
+    ):
+        print(msg)
+
+
+q = asyncio.run(main())
