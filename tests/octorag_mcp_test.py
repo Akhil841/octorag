c3240b6ea8cc8881c8af608d3331fa2b2f82b7aa
Akhil Pillai <avpillai@ucsd.edu>
2025-07-17 15:36:27 -0700

    final

--- tests/octorag_mcp_test.py
+++ tests/octorag_mcp_test.py
@@ -1,15 +1,13 @@
 from octorag.octorag_mcp_client import OctoRAG_MCP
 import asyncio
 
-model = OctoRAG_MCP()
+model = OctoRAG_MCP(debug=True)
 
 
 async def main():
     async for msg in model.query(
         (
-            "Write code in Python to create a HTTP server that can handle GET and POST requests"
-            " It should be able to retrieve stats of NBA players from the NBA API when asked."
-            " And it should predict the probability of a team winning a game against another team, based on the stats of the players."
+            "Write code in Rust for a raytracer. Use existing libraries from GitHub to handle the raytracing logic, I want a simple example that renders a sphere.",
         )
     ):
         print(msg)
